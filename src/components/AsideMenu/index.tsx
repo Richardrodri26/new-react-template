91f4bd2770850a3ff2c89e824a5696479f26fd50
RichardRodri26 <richardmanuel811@gmail.com>
2024-12-05 21:50:30 -0500

    change menu layout

--- src/components/AsideMenu/index.tsx
+++ src/components/AsideMenu/index.tsx
@@ -58,15 +58,16 @@ const asideMenuItems: IAsideMenuItem[] = [
 export const AsideMenu = () => {
 
   return (
-    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+    <aside className="min-w-14 flex-row border-r bg-background sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-        <a
-          href="#"
-          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+        <Link
+          to="/dashboard"
+          className="group flex px-2.5 h-9 min-w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:min-w-8 md:text-base"
         >
           <Home className="h-4 w-4 transition-all group-hover:scale-110" />
+          <p>SELLER</p>
           <span className="sr-only">Seller</span>
-        </a>
+        </Link>
         {
           asideMenuItems.map((item) => (
             <Fragment key={item.url}>
@@ -75,10 +76,11 @@ export const AsideMenu = () => {
                   <TooltipTrigger asChild>
                     <Link
                       to={item.url}
-                      className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                      className="flex h-9 min-w-9 w-full items-center justify-start gap-5 rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:min-w-8"
                     >
                       {/* <Home className="h-5 w-5" /> */}
                       {item.icon}
+                      {item.title}
                       <span className="sr-only">{item.title}</span>
                     </Link>
                   </TooltipTrigger>
